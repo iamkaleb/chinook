92d4ac3fcb488b563ce19e35e301d463ee729d3c
iamkaleb <kalebdmoran@gmail.com>
2020-10-10 16:51:46 -0500

    initial commit

---
+++ chinook.session.sql
@@ -0,0 +1,17 @@
+1.
+SELECT 
+    FirstName,
+    LastName,
+    CustomerId
+FROM Customer
+WHERE NOT Country='USA'
+
+2.
+SELECT 
+    FirstName,
+    LastName,
+    CustomerId
+FROM Customer
+WHERE Country='Brazil'
+
+3.
